19fa5cec7ff3728ae1957233911ac99136311aab
Jacob Meyer <jacobpmeyer87@gmail.com>
2022-04-21 13:26:08 -0500

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Jacob Meyer on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
